c8df75e347d77751fe922592e24e1564a9e17768
Hirotaka Imagawa <imosan@mac.com>
2023-12-04 17:45:08 +0900

    remove blikCollection definition

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,18 +2,6 @@
 import { z, defineCollection } from 'astro:content';
 
 // 2. コレクションを定義
-const blikCollection = defineCollection({
-    type: 'content', // v2.5.0以降
-    schema: z.object({
-        title: z.string(),
-        pubDate: z.date(),
-        tags: z.array(z.string()),
-        image: z.string().optional(),
-        description: z.string(),
-        seqNum: z.number(),
-    }),
-});
-
 const blogCollection = defineCollection({
     type: 'content', // v2.5.0以降
     schema: z.object({
@@ -28,6 +16,5 @@ const blogCollection = defineCollection({
 // 3. コレクションを登録するために、単一の`collections`オブジェクトをエクスポート
 //このキーは、"src/content"のコレクションのディレクトリ名と一致する必要があります。
 export const collections = {
-    'blik': blikCollection,
     'blog': blogCollection,
 };
